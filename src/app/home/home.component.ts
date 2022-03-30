7978e957d8c09b7d77cb7b2679c950b1845a5538
MAC <Fadi Khan>
2022-03-30 20:31:36 +0500

    route adjusted according to permissions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor(private _auth: AuthService, private router: Router) {
 
-  ngOnInit(): void {
-  }
+    }
 
+    ngOnInit(): void {
+    }
+
+    redirectToAllowedModule() {
+        if (this._auth.checkPermissions('dashboard', 'read')) {
+            this.router.navigate(['/dashboard']);
+        } else if (this._auth.checkPermissions('administration', 'read')) {
+            this.router.navigate(['/administration/users']);
+        } else if (this._auth.checkPermissions('delivery-list', 'read')) {
+            this.router.navigate(['/delivery-list/delivery']);
+        } else if (this._auth.checkPermissions('fsc', 'read')) {
+            this.router.navigate(['/fsc/customer-orders']);
+        } else if (this._auth.checkPermissions('rod', 'read')) {
+            this.router.navigate(['/rod']);
+        } else if (this._auth.checkPermissions('history', 'read')) {
+            this.router.navigate(['/history']);
+        } else if (this._auth.checkPermissions('phonebook', 'read')) {
+            this.router.navigate(['/phonebook/customers']);
+        } else if (this._auth.checkPermissions('accounts', 'read')) {
+            this.router.navigate(['/accounts']);
+        }
+    }
 }
